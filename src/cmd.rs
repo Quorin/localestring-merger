ed747c21faede3cd24123023ce35b70a476b7b5a
Quorin <xquoris@gmail.com>
2021-01-29 20:01:05 +0100

    cli commands refactor

---
+++ src/cmd.rs
@@ -0,0 +1,84 @@
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::{Input, Select};
+use std::fmt::{Display, Formatter};
+
+pub enum Action {
+    Merge,
+    Convert,
+    FindMissing,
+}
+
+impl Display for Action {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Action::Merge => write!(f, "{}", "Merge translations"),
+            Action::Convert => write!(f, "{}", "Convert old file"),
+            Action::FindMissing => write!(f, "{}", "Find missing translations"),
+        }
+    }
+}
+
+impl From<usize> for Action {
+    fn from(v: usize) -> Self {
+        match v {
+            0 => Action::Merge,
+            1 => Action::Convert,
+            2 => Action::FindMissing,
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub fn run() -> std::io::Result<()> {
+    println!("Choose action:");
+
+    let select_items = vec![Action::Merge, Action::Convert, Action::FindMissing];
+
+    let option: Action = Select::with_theme(&ColorfulTheme::default())
+        .items(&select_items)
+        .default(0)
+        .interact()?
+        .into();
+
+    match option {
+        Action::Merge => {
+            let cur_file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename containing current translations")
+                .default("locale_string.txt".into())
+                .interact_text()?;
+
+            let newer_file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename containing newer translations")
+                .default("locale_string2.txt".into())
+                .interact_text()?;
+
+            let save_file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename to which new translations will be saved")
+                .default("locale_string_new.txt".into())
+                .interact_text()?;
+
+            // merge
+        }
+        Action::Convert => {
+            let old_file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename containing old translations")
+                .default("locale_string.txt".into())
+                .interact_text()?;
+
+            let new_file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename to which converted translations will be saved")
+                .default("locale_string_new.txt".into())
+                .interact_text()?;
+
+            // convert
+        }
+        Action::FindMissing => {
+            let file: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt("Enter the filename containing translations")
+                .default("locale_string.txt".into())
+                .interact_text()?;
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,11 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-use dialoguer::theme::ColorfulTheme;
-use dialoguer::{Input, Select};
+use crate::cmd::run;
 
+mod cmd;
 mod parse;
 mod section;
 
 fn main() -> std::io::Result<()> {
-    println!("Choose action:");
-
-    let select_items = vec![
-        "Merge translations",
-        "Convert old file",
-        "Find missing translations",
-    ];
-
-    let option = Select::with_theme(&ColorfulTheme::default())
-        .items(&select_items)
-        .default(0)
-        .interact();
-
-    match option {
-        Ok(index) => {
-            match index {
-                0 => {
-                    let cur_file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Enter the filename containing current translations")
-                        .default("locale_string.txt".into())
-                        .interact_text()?;
-
-                    let newer_file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Enter the filename containing newer translations")
-                        .default("locale_string2.txt".into())
-                        .interact_text()?;
-
-                    let save_file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Enter the filename to which new translations will be saved")
-                        .default("locale_string_new.txt".into())
-                        .interact_text()?;
-
-                    // merge
-                }
-                1 => {
-                    let old_file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Enter the filename containing old translations")
-                        .default("locale_string.txt".into())
-                        .interact_text()?;
-
-                    let new_file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt(
-                            "Enter the filename to which converted translations will be saved",
-                        )
-                        .default("locale_string_new.txt".into())
-                        .interact_text()?;
-
-                    // convert
-                }
-                2 => {
-                    let file: String = Input::with_theme(&ColorfulTheme::default())
-                        .with_prompt("Enter the filename containing translations")
-                        .default("locale_string.txt".into())
-                        .interact_text()?;
-                }
-                _ => {}
-            }
-        }
-        _ => {}
-    }
-
-    Ok(())
+    run()
 }
